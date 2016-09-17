18c408eec13681ec54c5895df8fcdbe993d336bb
Jonathan A. Kollasch <jakllsch@kollasch.net>
2016-09-17 13:08:27 -0500

    Add caps lock led swapping for Satan-GH60 and KC60

--- keyboard/gh60/led.c
+++ keyboard/gh60/led.c
@@ -19,16 +19,47 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "stdint.h"
 #include "led.h"
 
+#ifdef BOOTMAGIC_ENABLE
+#include "keymap.h"
+extern keymap_config_t keymap_config;
+#endif
+
+#define K30_LED_DDR DDRB
+#define K30_LED_PORT PORTB
+#define K30_LED_PIN PINB
+#define K30_LED_BIT PB2
+
+#define K58_LED_DDR DDRB
+#define K58_LED_PORT PORTB
+#define K58_LED_PIN PINB
+#define K58_LED_BIT PB6
 
 void led_set(uint8_t usb_led)
 {
     if (usb_led & (1<<USB_LED_CAPS_LOCK)) {
-        // output low
-        DDRB |= (1<<2);
-        PORTB &= ~(1<<2);
+#if defined(GH60_REV_CHN) || defined(GH60_REV_KC60)
+        if (keymap_config.swap_control_capslock || keymap_config.capslock_to_control) {
+            // output high
+            DDRB |= (1<<6);
+            PORTB |= (1<<6);
+        } else
+#endif
+        {
+            // output low
+            DDRB |= (1<<2);
+            PORTB &= ~(1<<2);
+        }
     } else {
-        // Hi-Z
-        DDRB &= ~(1<<2);
-        PORTB &= ~(1<<2);
+#if defined(GH60_REV_CHN) || defined(GH60_REV_KC60)
+        if (keymap_config.swap_control_capslock || keymap_config.capslock_to_control) {
+            DDRB |= (1<<6);
+            PORTB &= ~(1<<6);
+        } else
+#endif
+        {
+            // Hi-Z
+            DDRB &= ~(1<<2);
+            PORTB &= ~(1<<2);
+        }
     }
 }
